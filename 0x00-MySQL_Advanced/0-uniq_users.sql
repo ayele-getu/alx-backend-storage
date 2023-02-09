9b7073e80a1f7b097ce0b96524f992c433e11c77
ayele-getu <ayelegetu44@gmail.com>
2023-02-09 09:48:49 +0300

    create table

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- this script creats a table called user.
 -- the table hase three coumns called id, email and name.
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 email VARCHAR(255) NOT NULL UNIQUE,
 name VARCHAR(255)
