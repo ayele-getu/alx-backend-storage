7e3e96c64248b985513568e5fd77ceb2d6eceebf
ayele-getu <ayelegetu44@gmail.com>
2023-02-09 09:56:31 +0300

    add one more column

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,6 +1,6 @@
 -- this script creats a table called user.
 -- the table hase four columns called id, email, name and country.
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 email VARCHAR(255) NOT NULL UNIQUE,
 name VARCHAR(255),
